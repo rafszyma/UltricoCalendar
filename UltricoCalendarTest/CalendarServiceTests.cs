6ba87c00d10172110fb6bea3084ec13b47cea793
Rafal <rafalaszymala@gmail.com>
2019-05-28 09:21:29 +0200

    added some test headers

--- UltricoCalendarTest/CalendarServiceTests.cs
+++ UltricoCalendarTest/CalendarServiceTests.cs
@@ -7,12 +7,19 @@ namespace UltricoCalendarTest
     {
         [Fact]public void TestIfAddsEvent(){}
         [Fact]public void TestIfGetsEvent(){}
-        
         [Fact]public void TestIfDeletesEvent(){}
-        
         [Fact]public void TestIfEditsEvent(){}
         
         [Fact]public void TestIfAddsEventSeries(){}
-        [Fact]public void TestIfAddsGetSeries(){}
+        [Fact]public void TestIfGetsEventSeries(){}
+        
+        [Fact]public void TestIfDeletesEventSeries(){}
+        [Fact]public void TestIfEditsEventSeries(){}
+        
+        [Fact]public void TestIfDeletedEventFromSeries(){}
+        [Fact]public void TestIfEditEventFromSeries(){}
+        
+        [Fact]public void TestIfGetsEditedEventFromSeries(){}
+        [Fact]public void TestIfGetsCalendarMetadata(){}
     }
 }
